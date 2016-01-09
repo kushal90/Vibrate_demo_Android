ed994153d124eaed65c99c6ab7c53cb81692e6fb
Kushal Kumar <k.kumar13@hotmail.com>
2016-01-10 00:16:38 +0200

    The code.
    Its the full working code of the demo.

--- A1/app/src/main/java/mobandsoccomp/a1/MainActivity.java
+++ A1/app/src/main/java/mobandsoccomp/a1/MainActivity.java
@@ -1,15 +1,22 @@
 package mobandsoccomp.a1;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +24,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        addListenerOnButton();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    public void addListenerOnButton() {
+
+        imageButton = (ImageButton) findViewById(R.id.vibbut);
+
+       /* imageButton.setOnTouchListener(new View.OnTouchListener() {
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+
+                    vibrator.vibrate(3000);
+                    return true;
+                }
+                else {
+                    vibrator.cancel();
+                }
+                return false;
+            }
+        });*/
+
+        imageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(300);
             }
         });
+
     }
 
     @Override
